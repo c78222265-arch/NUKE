6e41cc37e552d4537058d9241db020184d11f566
c78222265-arch <c78222265@gmail.com>
2026-06-06 11:01:49 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,43 @@
+# DONT TOUCH IT
+
+SERVER_CONFIG = {
+    "new_name":" ZYRO PAPA",  
+    "new_description": "FUCKED BY ZYRO",  
+    "new_icon": "https://i.imgur.com/aL0Q8ty.jpeg"
+}
+
+
+WEBHOOK_CONFIG = {
+    "default_name": "FUCK BY ZYRO",   
+}
+
+
+AUTO_RAID_CONFIG = {
+    'num_channels':250,  
+    'channel_type': 'text',   
+    'channel_name': 'ZYRO PAPA FUCK BY ZYRO',  
+    'num_messages': 100000000,   
+    'message_content':  'FUCKED BY ZYRO SUCKING YOU MOM ASS @everyone https://discord.gg/yPbbtWcNX' 
+}
+
+NO_BAN_KICK_ID = {
+    927989800387096586,       
+    1294173092234526770,     
+}
+
+BOT_PRESENCE = {
+    "type": "watching", 
+    "text": "text" 
+}
+
+EMBED_CONFIG = {
+    "title": "ZYRO  DADDY IS HERE",    
+    "description": "SUCK DADDY'S DICK BITCH",            
+    "color": 0xFF5733,      
+    "fields": [
+        {"name": ".GG/REX // 666", "value": ".GG/REX // 666", "inline": False},     
+        {"name": ".GG/REX // 666", "value": ".GG/REX // 666", "inline": False},
+        {"name": ".GG/REX // 666", "value": ".GG/REX // 666", "inline": False},    
+    ],
+        "footer": "",     
+}
